94a649b0f1d812ea7fc7772fcdc1e9dafc8a0250
Sergey Bugaev <bugaevc@gmail.com>
2017-04-24 22:38:18 +0300

    Use working CG/NS declarations

--- include/Foundation/NSGeometry.h
+++ include/Foundation/NSGeometry.h
@@ -15,7 +15,7 @@ typedef struct CGPoint CGPoint;
 typedef struct CGRect CGRect;
 typedef struct CGSize CGSize;
 
-#if __LP64__ || TARGET_OS_EMBEDDED || TARGET_OS_IPHONE || TARGET_OS_WIN32 || NS_BUILD_32_LIKE_64
+#if __LP64__ || TARGET_OS_EMBEDDED || TARGET_OS_IPHONE || TARGET_OS_WIN32 || NS_BUILD_32_LIKE_64 || DARLING
 
 typedef CGPoint NSPoint;
 
