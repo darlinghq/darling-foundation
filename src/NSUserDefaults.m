540e52e23966d499e249ec218943a303d7e00ded
Sergey Bugaev <bugaevc@gmail.com>
2018-08-14 11:07:05 +0300

    Fix setting NSLocale in NSUserDefaults

--- src/NSUserDefaults.m
+++ src/NSUserDefaults.m
@@ -37,7 +37,7 @@ static dispatch_queue_t synchronizeQueue;
         standardDefaults = [[NSUserDefaults alloc] initWithUser:NSUserName()];
         _startSynchronizeTimer(standardDefaults);
         [standardDefaults setObject:[NSLocale preferredLanguages] forKey:@"AppleLanguages"];
-        [standardDefaults setObject:[NSLocale systemLocale] forKey:@"AppleLocale"];
+        [standardDefaults setObject:[[NSLocale systemLocale] languageCode] forKey:@"AppleLocale"];
     }
     pthread_mutex_unlock(&defaultsLock);
     return standardDefaults;
