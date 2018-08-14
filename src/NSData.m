darlinghq/darling-foundation

commit abea16690daae4e8c0579291d03e1fd3ac89117c

Stub some missing APIs

src/NSData.m
@@ -23,9 +23,26 @@
 #import <stdlib.h>
 #import <sys/mman.h>
 
+typedef void (^NSDataDeallocator)(void *bytes, NSUInteger length);
+
+FOUNDATION_EXPORT const NSDataDeallocator NSDataDeallocatorVM = ^(void *bytes, NSUInteger length) {
+    // TODO
+};
+
+FOUNDATION_EXPORT const NSDataDeallocator NSDataDeallocatorUnmap = ^(void *bytes, NSUInteger length) {
+    munmap(bytes, length);
+};
+
+FOUNDATION_EXPORT const NSDataDeallocator NSDataDeallocatorFree = ^(void *bytes, NSUInteger length) {
+    free(bytes);
+};
+
+FOUNDATION_EXPORT const NSDataDeallocator NSDataDeallocatorNone = ^(void *bytes, NSUInteger length) {
+    // do nothing
+};
 
 @interface NSData (NSData)
-- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(void (^)(void *bytes, NSUInteger length))deallocator;
+- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(NSDataDeallocator)deallocator;
 - (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy freeWhenDone:(BOOL)shouldFree bytesAreVM:(BOOL)vm;
 @end
 
@@ -46,7 +63,7 @@ CF_PRIVATE
     union {
         unsigned char _space[NSCONCRETEDATA_BUFFER_SIZE];
         /* 12 makes a full allocation size of 32 bytes */
-        void (^_deallocator)(void *buffer, NSUInteger size);
+        NSDataDeallocator _deallocator;
     } _u;
 }
 
@@ -113,7 +130,7 @@ CF_PRIVATE
     }
 }
 
-- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(void (^)(void *, NSUInteger))deallocator
+- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(NSDataDeallocator)deallocator
 {
     if (length == 0)
     {
@@ -135,7 +152,7 @@ SINGLETON_RR()
 
 
 @implementation NSConcreteData
-- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(void (^)(void *, NSUInteger))deallocator
+- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(NSDataDeallocator)deallocator
 {
     self = [super init];
 
@@ -174,9 +191,7 @@ SINGLETON_RR()
                 [NSException raise:NSInvalidArgumentException format:@"Length too great for NSData"];
                 return nil;
             }
-            _u._deallocator = [^(void *buffer, NSUInteger size) {
-                free(buffer);
-            } copy];
+            _u._deallocator = NSDataDeallocatorFree;
         }
         memcpy(_bytes, bytes, length);
     }
@@ -1010,7 +1025,7 @@ OBJC_PROTOCOL_IMPL_PUSH
 }
 OBJC_PROTOCOL_IMPL_POP
 
-- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(void (^)(void *bytes, NSUInteger length))deallocator
+- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(NSDataDeallocator)deallocator
 {
     NSRequestConcreteImplementation();
     [self release];
@@ -1019,20 +1034,16 @@ OBJC_PROTOCOL_IMPL_POP
 
 - (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy freeWhenDone:(BOOL)shouldFree bytesAreVM:(BOOL)vm
 {
-    void (^deallocator)(void *bytes, NSUInteger length) = nil;
+    NSDataDeallocator deallocator;
     if (shouldFree)
     {
         if (vm)
         {
-            deallocator = ^void(void *bytesToDealloc, NSUInteger lengthToDealloc) {
-                munmap(bytesToDealloc, lengthToDealloc);
-            };
+            deallocator = NSDataDeallocatorUnmap;
         }
         else
         {
-            deallocator = ^void(void *bytesToDealloc, NSUInteger lengthToDealloc) {
-                free(bytesToDealloc);
-            };
+            deallocator = NSDataDeallocatorFree;
         }
     }
     return [self initWithBytes:bytes length:length copy:shouldCopy deallocator:deallocator];
@@ -1285,7 +1296,7 @@ static uint8_t base64EncodeLookup[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklm
 
 @implementation NSConcreteMutableData
 
-- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(void (^)(void *bytes, NSUInteger length))deallocator
+- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(NSDataDeallocator)deallocator
 {
     NSCapacityCheck(length, 0x80000000, @"Too huge of data length");
 
@@ -1593,7 +1604,7 @@ static void NSPurgeableDataStorageDiscard(NSPurgeableDataStorage *storage, NSUIn
     return self;
 }
 
-- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(void (^)(void *bytes, NSUInteger length))deallocator
+- (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy deallocator:(NSDataDeallocator)deallocator
 {
     NSCapacityCheck(length, 0x80000000, @"absurd capacity: %llu", (unsigned long long)length);
 

CMakeLists.txt
@@ -99,6 +99,7 @@ set(foundation_sources
 	src/NSKeyValueProperty.m
 	src/NSKeyValueUnnestedProperty.m
 	src/NSLikePredicateOperator.m
+	src/NSLinguisticTagger.m
 	src/NSLinkCheckingResult.m
 	src/NSLocale.m
 	src/NSLocalizableString.m
@@ -174,6 +175,7 @@ set(foundation_sources
 	src/NSUserDefaults.m
 	src/NSURLAuthenticationChallenge.m
 	src/NSURLCache.m
+	src/NSURLComponents.m
 	src/NSURLConnection.m
 	src/NSURLCredential.m
 	src/NSURLCredentialStorage.m

include/Foundation/NSCalendar.h
@@ -49,13 +49,32 @@ enum {
     NSUndefinedDateComponent = NSIntegerMax
 };
 
+typedef NSString *NSCalendarIdentifier;
+
+const NSCalendarIdentifier NSCalendarIdentifierGregorian;
+const NSCalendarIdentifier NSCalendarIdentifierISO8601;
+const NSCalendarIdentifier NSCalendarIdentifierBuddhist;
+const NSCalendarIdentifier NSCalendarIdentifierChinese;
+const NSCalendarIdentifier NSCalendarIdentifierCoptic;
+const NSCalendarIdentifier NSCalendarIdentifierEthiopicAmeteAlem;
+const NSCalendarIdentifier NSCalendarIdentifierEthiopicAmeteMihret;
+const NSCalendarIdentifier NSCalendarIdentifierHebrew;
+const NSCalendarIdentifier NSCalendarIdentifierIndian;
+const NSCalendarIdentifier NSCalendarIdentifierIslamic;
+const NSCalendarIdentifier NSCalendarIdentifierIslamicCivil;
+const NSCalendarIdentifier NSCalendarIdentifierIslamicTabular;
+const NSCalendarIdentifier NSCalendarIdentifierIslamicUmmAlQura;
+const NSCalendarIdentifier NSCalendarIdentifierJapanese;
+const NSCalendarIdentifier NSCalendarIdentifierPersian;
+const NSCalendarIdentifier NSCalendarIdentifierRepublicOfChina;
+
 @interface NSCalendar : NSObject <NSCopying, NSSecureCoding>
 
 + (id)currentCalendar;
 + (id)autoupdatingCurrentCalendar;
 
-- (id)initWithCalendarIdentifier:(NSString *)ident;
-- (NSString *)calendarIdentifier;
+- (id)initWithCalendarIdentifier:(NSCalendarIdentifier)ident;
+- (NSCalendarIdentifier)calendarIdentifier;
 - (void)setLocale:(NSLocale *)locale;
 - (NSLocale *)locale;
 - (void)setTimeZone:(NSTimeZone *)tz;

include/Foundation/NSURLComponents.h
@@ -1,0 +1,5 @@
+#import <Foundation/NSObject.h>
+
+@interface NSURLComponents : NSObject
+// TODO
+@end

src/NSCalendar.m
@@ -21,6 +21,24 @@ OBJC_PROTOCOL_IMPL_POP
 @end
 
 
+const NSCalendarIdentifier NSCalendarIdentifierGregorian = @"NSCalendarIdentifierGregorian";
+const NSCalendarIdentifier NSCalendarIdentifierISO8601 = @"NSCalendarIdentifierISO8601";
+const NSCalendarIdentifier NSCalendarIdentifierBuddhist = @"NSCalendarIdentifierBuddhist";
+const NSCalendarIdentifier NSCalendarIdentifierChinese = @"NSCalendarIdentifierChinese";
+const NSCalendarIdentifier NSCalendarIdentifierCoptic = @"NSCalendarIdentifierCoptic";
+const NSCalendarIdentifier NSCalendarIdentifierEthiopicAmeteAlem = @"NSCalendarIdentifierEthiopicAmeteAlem";
+const NSCalendarIdentifier NSCalendarIdentifierEthiopicAmeteMihret = @"NSCalendarIdentifierEthiopicAmeteMihret";
+const NSCalendarIdentifier NSCalendarIdentifierHebrew = @"NSCalendarIdentifierHebrew";
+const NSCalendarIdentifier NSCalendarIdentifierIndian = @"NSCalendarIdentifierIndian";
+const NSCalendarIdentifier NSCalendarIdentifierIslamic = @"NSCalendarIdentifierIslamic";
+const NSCalendarIdentifier NSCalendarIdentifierIslamicCivil = @"NSCalendarIdentifierIslamicCivil";
+const NSCalendarIdentifier NSCalendarIdentifierIslamicTabular = @"NSCalendarIdentifierIslamicTabular";
+const NSCalendarIdentifier NSCalendarIdentifierIslamicUmmAlQura = @"NSCalendarIdentifierIslamicUmmAlQura";
+const NSCalendarIdentifier NSCalendarIdentifierJapanese = @"NSCalendarIdentifierJapanese";
+const NSCalendarIdentifier NSCalendarIdentifierPersian = @"NSCalendarIdentifierPersian";
+const NSCalendarIdentifier NSCalendarIdentifierRepublicOfChina = @"NSCalendarIdentifierRepublicOfChina";
+
+
 @implementation NSAutoCalendar
 
 - (id)init
@@ -36,7 +54,7 @@ OBJC_PROTOCOL_IMPL_POP
     return self;
 }
 
-- (id)initWithCalendarIdentifier:(NSString *)ident
+- (id)initWithCalendarIdentifier:(NSCalendarIdentifier)ident
 {
     return [self init];
 }
@@ -48,7 +66,7 @@ OBJC_PROTOCOL_IMPL_POP
     [super dealloc];
 }
 
-- (NSString *)calendarIdentifier
+- (NSCalendarIdentifier)calendarIdentifier
 {
     return [cal calendarIdentifier];
 }

src/NSLinguisticTagger.m
@@ -1,0 +1,5 @@
+#import <Foundation/NSLinguisticTagger.h>
+
+@implementation NSLinguisticTagger
+// TODO
+@end

src/NSURLComponents.m
@@ -1,0 +1,5 @@
+#import <Foundation/NSURLComponents.h>
+
+@implementation NSURLComponents
+// TODO
+@end

commit acc894e2debca361392d041daacccd8a5e374394

Fix initializing NSData
This is a regression from abea16690daae4e8c0579291d03e1fd3ac89117c

src/NSData.m
@@ -1034,7 +1034,7 @@ OBJC_PROTOCOL_IMPL_POP
 
 - (id)initWithBytes:(void *)bytes length:(NSUInteger)length copy:(BOOL)shouldCopy freeWhenDone:(BOOL)shouldFree bytesAreVM:(BOOL)vm
 {
-    NSDataDeallocator deallocator;
+    NSDataDeallocator deallocator = nil;
     if (shouldFree)
     {
         if (vm)