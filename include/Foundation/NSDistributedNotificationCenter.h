ff0c404aa7d908eb5007a6e0785d1a25060e461e
Andrew Hyatt <ahyattdev@icloud.com>
2019-03-22 22:17:19 -0400

    NSDistributedNotificationCenter stubs

--- include/Foundation/NSDistributedNotificationCenter.h
+++ include/Foundation/NSDistributedNotificationCenter.h
@@ -1,8 +1,24 @@
+/*
+ This file is part of Darling.
+
+ Copyright (C) 2019 Lubos Dolezel
+
+ Darling is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Darling is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #import <Foundation/NSNotification.h>
-
-@class NSString, NSDictionary;
-
-NS_ASSUME_NONNULL_BEGIN
+#import <Foundation/NSString.h>
 
 typedef NSString * NSDistributedNotificationCenterType NS_EXTENSIBLE_STRING_ENUM;
 
@@ -19,30 +35,40 @@ typedef NS_OPTIONS(NSUInteger, NSDistributedNotificationOptions) {
     NSDistributedNotificationDeliverImmediately = (1UL << 0),
     NSDistributedNotificationPostToAllSessions = (1UL << 1)
 };
-static const NSDistributedNotificationOptions NSNotificationDeliverImmediately = NSDistributedNotificationDeliverImmediately;
-static const NSDistributedNotificationOptions NSNotificationPostToAllSessions = NSDistributedNotificationPostToAllSessions;
 
 @interface NSDistributedNotificationCenter : NSNotificationCenter
+{
+	BOOL _suspended;
+}
+
+@property BOOL suspended;
 
 + (NSDistributedNotificationCenter *)notificationCenterForType:(NSDistributedNotificationCenterType)notificationCenterType;
 
 + (NSDistributedNotificationCenter *)defaultCenter;
 
-- (void)addObserver:(id)observer selector:(SEL)selector name:(nullable NSNotificationName)name object:(nullable NSString *)object suspensionBehavior:(NSNotificationSuspensionBehavior)suspensionBehavior;
+- (void)addObserver:(id)observer
+	selector:(SEL)selector
+	name:(NSNotificationName)name
+	object:(NSString *)object
+	suspensionBehavior:(NSNotificationSuspensionBehavior)suspensionBehavior;
 
-- (void)postNotificationName:(NSNotificationName)name object:(nullable NSString *)object userInfo:(nullable NSDictionary *)userInfo deliverImmediately:(BOOL)deliverImmediately;
+- (void)postNotificationName:(NSNotificationName)name
+	object:(NSString *)object
+	userInfo:(NSDictionary *)userInfo
+	deliverImmediately:(BOOL)deliverImmediately;
 
-- (void)postNotificationName:(NSNotificationName)name object:(nullable NSString *)object userInfo:(nullable NSDictionary *)userInfo options:(NSDistributedNotificationOptions)options;
+- (void)postNotificationName:(NSNotificationName)name
+	object:(NSString *)object
+	userInfo:(NSDictionary *)userInfo
+	options:(NSDistributedNotificationOptions)options;
 
-@property BOOL suspended;
+- (void)addObserver:(id)observer selector:(SEL)aSelector name:(NSNotificationName)aName object:(NSString *)anObject;
 
-- (void)addObserver:(id)observer selector:(SEL)aSelector name:(nullable NSNotificationName)aName object:(nullable NSString *)anObject;
+- (void)postNotificationName:(NSNotificationName)aName object:(NSString *)anObject;
 
-- (void)postNotificationName:(NSNotificationName)aName object:(nullable NSString *)anObject;
-- (void)postNotificationName:(NSNotificationName)aName object:(nullable NSString *)anObject userInfo:(nullable NSDictionary *)aUserInfo;
-- (void)removeObserver:(id)observer name:(nullable NSNotificationName)aName object:(nullable NSString *)anObject;
+- (void)postNotificationName:(NSNotificationName)aName object:(NSString *)anObject userInfo:(NSDictionary *)aUserInfo;
+
+- (void)removeObserver:(id)observer name:(NSNotificationName)aName object:(NSString *)anObject;
 
 @end
-
-NS_ASSUME_NONNULL_END
-
--- src/NSDistributedNotificationCenter.m
+++ src/NSDistributedNotificationCenter.m
@@ -1,5 +1,81 @@
+/*
+ This file is part of Darling.
+
+ Copyright (C) 2019 Lubos Dolezel
+
+ Darling is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Darling is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #import <NSDistributedNotificationCenter.h>
 
 @implementation NSDistributedNotificationCenter
+
++ (NSDistributedNotificationCenter *)notificationCenterForType:(NSDistributedNotificationCenterType)notificationCenterType
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+	return nil;
+}
+
++ (NSDistributedNotificationCenter *)defaultCenter
+{
+	return nil;
+}
+
+- (void)addObserver:(id)observer
+	selector:(SEL)selector
+	name:(NSNotificationName)name
+	object:(NSString *)object
+	suspensionBehavior:(NSNotificationSuspensionBehavior)suspensionBehavior
+{
+	NSLog(@"-[NSDistributedNotificationCenter addObserver:selector:name:object:suspensionBehavior:");
+}
+
+- (void)postNotificationName:(NSNotificationName)name
+	object:(NSString *)object
+	userInfo:(NSDictionary *)userInfo
+	deliverImmediately:(BOOL)deliverImmediately
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)postNotificationName:(NSNotificationName)name
+	object:(NSString *)object
+	userInfo:(NSDictionary *)userInfo
+	options:(NSDistributedNotificationOptions)options
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+
+- (void)addObserver:(id)observer selector:(SEL)aSelector name:(NSNotificationName)aName object:(NSString *)anObject
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)postNotificationName:(NSNotificationName)aName object:(NSString *)anObject
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)postNotificationName:(NSNotificationName)aName object:(NSString *)anObject userInfo:(NSDictionary *)aUserInfo
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)removeObserver:(id)observer name:(NSNotificationName)aName object:(NSString *)anObject
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
 
 @end
