c5455620111b31008e6ab00cfcdedf343466f82b
TheBrokenRail <connor24nolan@live.com>
2020-05-11 21:10:32 -0400

    Move Cocotron Code To Foundation

--- include/Foundation/NSXMLNodeOptions.h
+++ include/Foundation/NSXMLNodeOptions.h
@@ -1,1 +1,4 @@
-../../../cocotron/Foundation/xml/NSXMLNodeOptions.h
+enum {
+    NSXMLDocumentTidyHTML = 1UL << 9,
+    NSXMLDocumentTidyXML = 1UL << 10
+};
--- src/NSSpellServer.m
+++ src/NSSpellServer.m
@@ -1,1 +1,6 @@
-../../cocotron/Foundation/NSSpellServer.m
+#import <Foundation/NSSpellServer.h>
+#import <Foundation/NSString.h>
+
+NSString *const NSGrammarRange=@"NSGrammarRange";
+NSString *const NSGrammarUserDescription=@"NSGrammarUserDescription";
+NSString *const NSGrammarCorrections=@"NSGrammarCorrections";
