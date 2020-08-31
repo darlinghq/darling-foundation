efcf08f812f69e0df5f7b6db6ce449e3a2485d5f
Ariel Abreu <facekapow@outlook.com>
2020-08-31 15:53:45 -0400

    NSDictionary: Use appropriate generic types instead of id

--- include/Foundation/NSDictionary.h
+++ include/Foundation/NSDictionary.h
@@ -1,41 +1,41 @@
 #import <Foundation/NSObject.h>
 #import <Foundation/NSEnumerator.h>
 
-@class NSArray, NSSet, NSString, NSURL;
+@class NSArray<ObjectType>, NSSet<ObjectType>, NSString, NSURL;
 
 @interface NSDictionary<__covariant KeyType, __covariant ObjectType> : NSObject <NSCopying, NSMutableCopying, NSSecureCoding, NSFastEnumeration>
 
 - (NSUInteger)count;
-- (id)objectForKey:(id)aKey;
-- (NSEnumerator *)keyEnumerator;
+- (ObjectType)objectForKey:(KeyType)aKey;
+- (NSEnumerator<KeyType> *)keyEnumerator;
 
 @end
 
 @interface NSDictionary<KeyType, ObjectType> (NSExtendedDictionary)
 
-- (NSArray *)allKeys;
-- (NSArray *)allKeysForObject:(id)anObject;
+- (NSArray<KeyType> *)allKeys;
+- (NSArray<KeyType> *)allKeysForObject:(ObjectType)anObject;
 
-- (NSArray *)allValues;
+- (NSArray<ObjectType> *)allValues;
 - (NSString *)description;
 - (NSString *)descriptionInStringsFileFormat;
 - (NSString *)descriptionWithLocale:(id)locale;
 - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level;
 - (BOOL)isEqualToDictionary:(NSDictionary *)otherDictionary;
-- (NSEnumerator *)objectEnumerator;
-- (NSArray *)objectsForKeys:(NSArray *)keys notFoundMarker:(id)marker;
+- (NSEnumerator<ObjectType> *)objectEnumerator;
+- (NSArray<ObjectType> *)objectsForKeys:(NSArray<KeyType> *)keys notFoundMarker:(id)marker;
 - (BOOL)writeToFile:(NSString *)path atomically:(BOOL)useAuxiliaryFile;
 - (BOOL)writeToURL:(NSURL *)url atomically:(BOOL)atomically;
-- (NSArray *)keysSortedByValueUsingSelector:(SEL)comparator;
-- (void)getObjects:(id __unsafe_unretained [])objects andKeys:(id __unsafe_unretained [])keys;
-- (id)objectForKeyedSubscript:(id)key;
+- (NSArray<KeyType> *)keysSortedByValueUsingSelector:(SEL)comparator;
+- (void)getObjects:(ObjectType __unsafe_unretained [])objects andKeys:(KeyType __unsafe_unretained [])keys;
+- (ObjectType)objectForKeyedSubscript:(id)key;
 #if NS_BLOCKS_AVAILABLE
-- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL *stop))block;
-- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id key, id obj, BOOL *stop))block;
-- (NSArray *)keysSortedByValueUsingComparator:(NSComparator)cmptr;
-- (NSArray *)keysSortedByValueWithOptions:(NSSortOptions)opts usingComparator:(NSComparator)cmptr;
-- (NSSet *)keysOfEntriesPassingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate;
-- (NSSet *)keysOfEntriesWithOptions:(NSEnumerationOptions)opts passingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate;
+- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(KeyType key, ObjectType obj, BOOL *stop))block;
+- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(KeyType key, ObjectType obj, BOOL *stop))block;
+- (NSArray<KeyType> *)keysSortedByValueUsingComparator:(NSComparator)cmptr;
+- (NSArray<KeyType> *)keysSortedByValueWithOptions:(NSSortOptions)opts usingComparator:(NSComparator)cmptr;
+- (NSSet<KeyType> *)keysOfEntriesPassingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate;
+- (NSSet<KeyType> *)keysOfEntriesWithOptions:(NSEnumerationOptions)opts passingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate;
 #endif
 
 @end
@@ -43,18 +43,18 @@
 @interface NSDictionary<KeyType, ObjectType> (NSDictionaryCreation)
 
 + (instancetype)dictionary;
-+ (instancetype)dictionaryWithObject:(id)object forKey:(id <NSCopying>)key;
-+ (instancetype)dictionaryWithObjects:(const id [])objects forKeys:(const id <NSCopying> [])keys count:(NSUInteger)cnt;
-+ (instancetype)dictionaryWithObjectsAndKeys:(id)firstObject, ... NS_REQUIRES_NIL_TERMINATION;
-+ (instancetype)dictionaryWithDictionary:(NSDictionary *)dict;
-+ (instancetype)dictionaryWithObjects:(NSArray *)objects forKeys:(NSArray *)keys;
++ (instancetype)dictionaryWithObject:(ObjectType)object forKey:(KeyType <NSCopying>)key;
++ (instancetype)dictionaryWithObjects:(const ObjectType [])objects forKeys:(const KeyType <NSCopying> [])keys count:(NSUInteger)cnt;
++ (instancetype)dictionaryWithObjectsAndKeys:(ObjectType)firstObject, ... NS_REQUIRES_NIL_TERMINATION;
++ (instancetype)dictionaryWithDictionary:(NSDictionary<KeyType, ObjectType> *)dict;
++ (instancetype)dictionaryWithObjects:(NSArray<ObjectType> *)objects forKeys:(NSArray<KeyType> *)keys;
 + (instancetype)dictionaryWithContentsOfFile:(NSString *)path;
 + (instancetype)dictionaryWithContentsOfURL:(NSURL *)url;
-- (instancetype)initWithObjects:(const id [])objects forKeys:(const id <NSCopying> [])keys count:(NSUInteger)cnt;
-- (instancetype)initWithObjectsAndKeys:(id)firstObject, ... NS_REQUIRES_NIL_TERMINATION;
-- (instancetype)initWithDictionary:(NSDictionary *)otherDictionary;
-- (instancetype)initWithDictionary:(NSDictionary *)otherDictionary copyItems:(BOOL)flag;
-- (instancetype)initWithObjects:(NSArray *)objects forKeys:(NSArray *)keys;
+- (instancetype)initWithObjects:(const ObjectType [])objects forKeys:(const KeyType <NSCopying> [])keys count:(NSUInteger)cnt;
+- (instancetype)initWithObjectsAndKeys:(ObjectType)firstObject, ... NS_REQUIRES_NIL_TERMINATION;
+- (instancetype)initWithDictionary:(NSDictionary<KeyType, ObjectType> *)otherDictionary;
+- (instancetype)initWithDictionary:(NSDictionary<KeyType, ObjectType> *)otherDictionary copyItems:(BOOL)flag;
+- (instancetype)initWithObjects:(NSArray<ObjectType> *)objects forKeys:(NSArray<KeyType> *)keys;
 - (instancetype)initWithContentsOfFile:(NSString *)path;
 - (instancetype)initWithContentsOfURL:(NSURL *)url;
 
@@ -62,37 +62,37 @@
 
 @interface NSMutableDictionary<KeyType, ObjectType> : NSDictionary<KeyType, ObjectType>
 
-- (void)removeObjectForKey:(id)aKey;
-- (void)setObject:(id)anObject forKey:(id <NSCopying>)aKey;
+- (void)removeObjectForKey:(KeyType)aKey;
+- (void)setObject:(ObjectType)anObject forKey:(KeyType <NSCopying>)aKey;
 
 @end
 
 @interface NSMutableDictionary<KeyType, ObjectType> (NSExtendedMutableDictionary)
 
-- (void)addEntriesFromDictionary:(NSDictionary *)otherDictionary;
+- (void)addEntriesFromDictionary:(NSDictionary<KeyType, ObjectType> *)otherDictionary;
 - (void)removeAllObjects;
-- (void)removeObjectsForKeys:(NSArray *)keyArray;
-- (void)setDictionary:(NSDictionary *)otherDictionary;
-- (void)setObject:(id)obj forKeyedSubscript:(id <NSCopying>)key;
+- (void)removeObjectsForKeys:(NSArray<KeyType> *)keyArray;
+- (void)setDictionary:(NSDictionary<KeyType, ObjectType> *)otherDictionary;
+- (void)setObject:(ObjectType)obj forKeyedSubscript:(KeyType <NSCopying>)key;
 
 @end
 
 @interface NSMutableDictionary<KeyType, ObjectType> (NSMutableDictionaryCreation)
 
-+ (id)dictionaryWithCapacity:(NSUInteger)numItems;
-- (id)initWithCapacity:(NSUInteger)numItems;
++ (instancetype)dictionaryWithCapacity:(NSUInteger)numItems;
+- (instancetype)initWithCapacity:(NSUInteger)numItems;
 
 @end
 
 @interface NSDictionary<KeyType, ObjectType> (NSSharedKeySetDictionary)
 
-+ (id)sharedKeySetForKeys:(NSArray *)keys;
++ (id)sharedKeySetForKeys:(NSArray<KeyType> *)keys;
 
 @end
 
 @interface NSMutableDictionary<KeyType, ObjectType> (NSSharedKeySetDictionary)
 
-+ (id)dictionaryWithSharedKeySet:(id)keyset;
++ (instancetype)dictionaryWithSharedKeySet:(id)keyset;
 
 @end
 
